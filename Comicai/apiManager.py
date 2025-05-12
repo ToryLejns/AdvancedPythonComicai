928e9fecc1e1420d02bcaec82c0374fa8f4b4e30
ToryLinux <ave.jam3@gmail.com>
2025-05-12 22:27:09 +0200

    cleaned up

---
+++ Comicai/apiManager.py
@@ -0,0 +1,5 @@
+def getTextToImage(prompt):
+    pass
+
+def getImageToImage(prompt, image):
+    pass
--- Comicai/dashboard.py
+++ Comicai/dashboard.py
@@ -1,4 +1,4 @@
-
+from calendar import error
 
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for, abort
@@ -6,8 +6,8 @@ from flask import (
 
 from Comicai.auth import login_required
 from Comicai.blobUtils import pil_image_to_blob
-from Comicai.characterClient import getCharacter
 from Comicai.db import get_db
+from Comicai.apiManager import *
 
 bp = Blueprint('dashboard', __name__)
 
@@ -15,13 +15,6 @@ bp = Blueprint('dashboard', __name__)
 @login_required
 def index():
     db = get_db()
-    # chracters = db.execute(
-    #     'SELECT p.id, title, image, created, author_id, username'
-    #     ' FROM character p JOIN user u ON p.author_id = u.id'
-    #     ' ORDER BY created DESC'
-    # ).fetchall()
-    #
-    # img = get_img(1)
 
     return render_template('dashboard/dashboard.html')
 
@@ -29,46 +22,27 @@ def index():
 @login_required
 def add_character():
     if request.method == 'POST':
-        # title = request.form['title']
-        # prompt = request.form['prompt']
+        name = request.form['name']
+        prompt = request.form['description']
+        picture = request.files.get("image")
         error = None
+        if not name:
+            error = 'Name is required!'
 
-        # image = getCharacter(prompt)
+        if not prompt:
+            error = 'description is required!'
 
-        # imageBinary = pil_image_to_blob(image)
+        if picture and picture.filename == "":
+            error = 'empty file!'
 
-        # if not title:
-        #     error = 'Title is required.'
+        if not error:
+            if not picture:
+                getTextToImage(prompt)
+            else:
+                getImageToImage(prompt, picture)
 
-        if error is not None:
-            flash(error)
-        else:
-            # db = get_db()
-            # db.execute(
-            #     'INSERT INTO character (title, image, author_id)'
-            #     ' VALUES (?, ?, ?)',
-            #     ("title", imageBinary, g.user['id'])
-            # )
-            # db.commit()
-            return redirect(url_for('dashboard/dashboard.html'))
 
     return render_template('dashboard/add_character.html')
-
-def get_img(id, check_author=True):
-    img = get_db().execute(
-        'SELECT p.id, title, image, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
-        ' WHERE p.id = ?',
-        (id,)
-    ).fetchone()
-
-    if img is None:
-        abort(404, f"Post id {id} doesn't exist.")
-
-    if check_author and img['author_id'] != g.user['id']:
-        abort(403)
-
-    return img
 
 @bp.route('/add_comic', methods=('GET', 'POST'))
 @login_required
